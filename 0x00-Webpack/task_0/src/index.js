73f3ab5795d6863c2e4d067c3d5f1d600fe560e0
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-05-12 19:01:26 +0300

    Task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+// add three parapgraphs
+$('body').append(
+    '<p>Holberton Dashboard</p>',
+    '<p>Dashboard data for the students</p>',
+    '<p>Copyright - Holberton School</p>'
+)
