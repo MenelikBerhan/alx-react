0bfce15a07f3de34a45b4f8c9e30b30851bcb45b
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-05-12 19:48:48 +0300

    Add semicolons

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -3,10 +3,10 @@ import $ from "jquery";
 // add three parapgraphs
 $('body').append(
     '<p>Holberton Dashboard</p>',
-)
+);
 $('body').append(
     '<p>Dashboard data for the students</p>',
-)
+);
 $('body').append(
     '<p>Copyright - Holberton School</p>'
-)
+);
