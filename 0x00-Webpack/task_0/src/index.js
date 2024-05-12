b5a5474871de4178ccd40face4b6c8059d44730f
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-05-12 20:00:37 +0300

    clean up

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,12 +1,6 @@
 import $ from "jquery";
 
 // add three parapgraphs
-$('body').append(
-    '<p>Holberton Dashboard</p>',
-);
-$('body').append(
-    '<p>Dashboard data for the students</p>',
-);
-$('body').append(
-    '<p>Copyright - Holberton School</p>'
-);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
